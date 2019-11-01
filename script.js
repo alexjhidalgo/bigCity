e370411a3b466dba23717e594839cc57fc4b39f3
aalexmann <alex.h@Alexs-MacBook.local>
2019-11-01 04:12:44 -0400

    added wireframe

---
+++ script.js
@@ -0,0 +1,60 @@
+// 'use strict'
+
+// function validateSignUpForm(){
+
+// }
+
+// function sendSignUpData(){
+//     //maybe just saves the name of user and displays it in the header and model
+// }
+
+// function moveToQuestionaire(){
+//     //hides other pages, displays questionaire page
+// }
+
+// function validateQuestionaireForm(){
+
+// }
+
+// function getCityWeatherData(){
+//     let spring = ""
+//     let summer = ""
+//     let autumn = ""
+//     let winter = ""
+// }
+
+// function getRentalPriceData(){
+//     let studio = ""
+//     let bdrm1 = ""
+//     let bdrm2 = ""
+//     let bdrm3 = ""
+// }
+
+// function getEmploymentData(){
+//     let jobNums = ""
+//     let medIncome = ""
+//     let jobGrowth = ""
+// }
+
+// function moveToModelPage(){
+//     //puts info from above three functions into the dynamic html code
+//     //hides other pages, displays model page
+// }
+
+// function downloadModel(){
+
+// }
+
+// function printModel(){
+
+// }
+
+// function startNewModel(){
+//     //empties all dynamic changes to html
+//     //hides other pages, displays landing page
+// }
+
+$(function runIt() {
+    $(document).ready() 
+    console.log('Locked n Loaded');
+  });
